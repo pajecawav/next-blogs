458c9b66bb858396680de72fdd7e4486f4ddc571
pajecawav <pajecawav@gmail.com>
2021-10-29 18:59:45 +0300

    feat: add edit post link to post page

--- src/pages/posts/[[...slug]].tsx
+++ src/pages/posts/[[...slug]].tsx
@@ -1,17 +1,26 @@
 import { Post } from "@/components/Post";
 import { UserLink } from "@/components/UserLink";
+import { useUser } from "@/hooks/useUser";
 import { formatDate } from "@/lib/dates";
 import { PostWithUserResponse } from "@/lib/schemas/post";
+import { Menu, Transition } from "@headlessui/react";
+import { DotsHorizontalIcon, PencilIcon } from "@heroicons/react/outline";
+import classNames from "classnames";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
+import Link from "next/link";
 import db from "prisma/client";
-import React from "react";
+import React, { Fragment } from "react";
 
 type Props = {
 	post: PostWithUserResponse;
 };
 
 const PostPage: React.FC<Props> = ({ post }) => {
+	const { user } = useUser();
+
+	const isMyPost = post.authorId === user?.id;
+
 	return (
 		<>
 			<Head>
@@ -20,7 +29,7 @@ const PostPage: React.FC<Props> = ({ post }) => {
 			<div className="pt-4 pb-4 px-6 bg-white shadow-sm">
 				<h1 className="text-4xl mb-3">{post.title}</h1>
 				<article className="bg-white prose-sm">
-					<div className="flex gap-5 items-center mb-8">
+					<div className="flex gap-4 items-center mb-8">
 						<span>
 							by{" "}
 							<UserLink username={post.author.username}>
@@ -35,6 +44,41 @@ const PostPage: React.FC<Props> = ({ post }) => {
 						>
 							{formatDate(post.createdAt)}
 						</span>
+						{isMyPost && (
+							<Menu as="div" className="relative">
+								<Menu.Button className="align-middle">
+									<DotsHorizontalIcon className="w-5 h-5 text-gray-400" />
+								</Menu.Button>
+								<Transition
+									as={Fragment}
+									enter="transition ease-out duration-50"
+									enterFrom="opacity-0 translate-y-0.5"
+									leave="transition ease-in duration-50"
+									leaveTo="opacity-0 translate-y-0.5"
+								>
+									<Menu.Items className="absolute right-0 origin-top-right flex flex-col shadow-lg z-10 py-1 px-2 w-40 border bg-white rounded-md">
+										<Menu.Item>
+											{({ active }) => (
+												<Link
+													href={`/edit-post/${post.id}`}
+												>
+													<a
+														className={classNames(
+															"flex gap-2 items-center text-left px-2 py-1 rounded-md hover:bg-gray-200",
+															active &&
+																"bg-gray-200"
+														)}
+													>
+														<PencilIcon className="inline w-4 h-4" />
+														<span>Edit</span>
+													</a>
+												</Link>
+											)}
+										</Menu.Item>
+									</Menu.Items>
+								</Transition>
+							</Menu>
+						)}
 					</div>
 					<Post>{post.body}</Post>
 				</article>
